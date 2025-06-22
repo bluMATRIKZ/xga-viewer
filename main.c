b826b0105f20227d15f079cde8f993f5973bd459
bluMATRIKZ <blumatrikz@gmail.com>
2025-06-22 13:40:32 -0700

    Add copyright info

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/*
+XGA Viewer - A image viewer for my 4 bit color image format
+Copyright (C) 2025 Connor Thomson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or 
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Compile: gcc viewer.c -o viewer `sdl2-config --cflags --libs`
+*/
+
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
